a47ca57b7fd11f01baaf9bafea15735140af68f5
ManuDev <vargas_manuel08@hotmail.com>
2025-02-05 22:32:05 -0500

    feat: Add styling and game logic for JS Game across multiple lessons Completed new course

---
+++ Formación Principiante en programación/Logica de programación/2034-logica-programacion-1-Aula5/app.js
@@ -0,0 +1,44 @@
+//Variables
+
+
+
+let numeroMinimoPosible = Math.floor(Math.random()*5)+1;
+let posiblesMaximos = [10, 100, 1000];
+let indiceMaximoAleatorio = Math.floor(Math.random() * 3);
+let numeroMaximoPosible = posiblesMaximos[indiceMaximoAleatorio];
+
+
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 6;
+
+
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre ${numeroMinimoPosible} y ${numeroMaximoPosible}  por favor:`));
+
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
+}
