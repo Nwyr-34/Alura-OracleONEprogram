4a2585bd8c7968b1de60f2c9b443f045634d3cd6
ManuDev <vargas_manuel08@hotmail.com>
2025-03-19 00:15:18 -0500

    feat: actualización de cursos formación principiante programación

---
+++ Formación Principiante en programación/Logica de programación/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,16 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Adivina el número secreto entre 1 y 10';
+
+function intentoDeUsuario() {
+    // let numeroSecreto = 7;
+    // let numeroUsuario = prompt('Introduce un número');
+    // if (numeroUsuario == numeroSecreto) {
+    //     alert('¡Has acertado!');
+    // } else {
+    //     alert('¡Has fallado!');
+    // }
+    alert('¡Has fallado!');
+}
